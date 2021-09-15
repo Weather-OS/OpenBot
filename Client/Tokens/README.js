41e29938619b26d60c7c6f40d6590444e8118aa4
Weather-OS <tpapata1386@gmail.com>
2021-09-15 22:15:16 +0430

    Update

--- Client/Tokens/README.js
+++ Client/Tokens/README.js
@@ -1,5 +1,5 @@
 //Change this file's name to DiscordToken.js
 
 //Then insert your discord token here:
-const TOKEN = "YOURTOKENHEREas";
+const TOKEN = "YOURTOKENHEREasd";
 module.exports = { TOKEN };
